5445c8a1bd0bfd832dc6d08cb134397886c95366
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-06-22 17:57:41 -0300

    Hard skills

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ document.addEventListener("scroll", (e) => {
 
 })
 
+const acordion = document.querySelectorAll(".accordion")
+
+
+acordion.forEach((element) => {
+    element.addEventListener("click", () => {
+
+        const ul = element.parentNode.querySelector("ul")
+        const iconAcordion = element.querySelector("i")
+        ul.classList.toggle("expand")
+
+
+        if(ul.classList.contains("expand")){
+            console.log(ul.classList.contains("expand"))
+            ul.style.height = ul.scrollHeight + 100 + "px"
+            ul.style.padding = "15px 0"
+            iconAcordion.classList.remove("ph-book-open")
+            iconAcordion.classList.add("ph-book-open-text")
+
+        }else{
+            ul.style.height = 0
+            ul.style.padding = "0"
+            iconAcordion.classList.add("ph-book-open")
+            iconAcordion.classList.remove("ph-book-open-text")
+
+        }
+    })
+})
+
